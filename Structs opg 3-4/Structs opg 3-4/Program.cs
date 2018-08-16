4821ec1e37f61e95da6a52e2ea1fbfca2dbad5c0
Frederik Koch <frederikhkoch@gmail.com>
2018-08-16 14:26:55 +0200

    Structs 2

---
+++ Structs opg 3-4/Structs opg 3-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_opg_3_4
+{
+    class Program
+    {
+        public struct Beer
+        {
+            public string Fabrikant;
+            public string Type;
+            public string Procent;
+            public string Volumen;
+        }
+        static void Main(string[] args)
+        {
+            Beer b1;
+            b1.Fabrikant = "Carlsberg";
+            b1.Type = "Classic";
+            b1.Procent = "6.3%";
+            b1.Volumen = "330ML";
+
+            Console.WriteLine(Udskriv(b1));
+
+        }
+        public static string Udskriv(Beer udskrivdata)
+        {
+            return "Fabrikant: " + udskrivdata.Fabrikant +
+                "\nType: " + udskrivdata.Type +
+                "\nProcent: " + udskrivdata.Procent +
+                "\nVolumen: " + udskrivdata.Volumen;
+        }
+    }
+}
