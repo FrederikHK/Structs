2f053847b138b3923834237cc10126deabfe5f80
Frederik Koch <frederikhkoch@gmail.com>
2018-08-22 08:33:26 +0200

    method program complete

---
+++ Fredag1708/Fredag1708/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fredag1708
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Hvad hedder du? :");
+            char[] schar = { ' ' };
+            name = Console.ReadLine().Trim();
+            string[] splitName = name.Split(schar, StringSplitOptions.RemoveEmptyEntries);
+            string totalName = "";
+            foreach (string xname in splitName)
+            {
+                totalName += UpperandLower(xname) + " "; //Join the names
+               
+            }
+            Console.WriteLine(totalName.TrimEnd());
+            //Here is our function(method because it's placed inside of the class - Main)
+            string UpperandLower(string iname)
+            {
+                if (iname.Length != 0)
+                {
+                    return iname.Substring(0, 1).ToUpper() + iname.Substring(1).ToLower();
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+        }
+    }
+}
---
+++ metoder/metoder/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metoder
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            string totalPerson = "";
+            string person;
+            string testPerson = "";
+            char[] schar = { ' ' };
+            List<string> stringList = new List<string>();
+            List<string> aliasList = new List<string>();
+            Dictionary<string, int> newDictionary = new Dictionary<string, int>();
+            
+            do
+            {
+                Console.WriteLine("Vælg om du vil: \n'a' Indsætte ny person eller \n'b' Printe listen ud med personer.\n'q' For at afslutte");
+
+                string svar = Console.ReadLine();
+                switch (svar)
+                {
+                    case "a":
+                        
+                        Console.Write("Indtast venligst dit navn: ");
+                        person = Console.ReadLine().Trim();
+                        string[] splitName = person.Split(schar, StringSplitOptions.RemoveEmptyEntries);
+                      
+                        totalPerson = "";
+                        
+                        foreach (string xpersons in splitName)
+                        {
+                            if (xpersons != splitName[splitName.Length - 1])
+                            {
+                                testPerson += Persons(xpersons) + " ";
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            
+                        }
+                        string alias = splitName[0].Substring(0, 1) + splitName[splitName.Length - 1].Substring(0, 1);
+                        totalPerson = Persons(splitName[splitName.Length - 1]) + ", " + testPerson + "dit alias er: " + alias;
+                        stringList.Add(totalPerson.TrimEnd());
+                        if (newDictionary.ContainsKey(alias))
+                        {
+                            newDictionary[alias]++;
+                        }
+                        else
+                        {
+                            newDictionary.Add(alias, 1);
+                        }
+                        break;
+
+                    case "b":
+                        stringList.Sort();
+                        foreach (var element in stringList)
+                        {
+                                Console.WriteLine(element);
+                        }
+                        foreach (var newitem in newDictionary)
+                        {
+                            Console.WriteLine(newitem);                        
+                        }
+
+                        break;
+
+                    case "q":
+                        loop = false;
+                        break;
+                }
+                
+            } while (loop);
+        }
+        public static string Persons(string xName)
+        {
+            if (xName.Length != 0)
+            {
+                return xName.Substring(0, 1).ToUpper() + xName.Substring(1).ToLower();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+    
+
+}
